21c60ae4d23e1f183aa8de5bc167ec3f219a688c
ketanbhandekar <ketanbhandekar33@gmail.com>
2024-11-18 17:08:11 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,8 @@
+""" PDF TO IMAGE"""
+
+from pdf2image import convert_from_path
+
+old_pdf=convert_from_path("test.pdf",poppler_path=r"C:\Users\Admin\Desktop\Python_api\poppler-24.02.0\Library\bin")
+
+for i in range(len(old_pdf)):
+    old_pdf[i].save("new" + str(i)+ ".jpg","JPEG")
